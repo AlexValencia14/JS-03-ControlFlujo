a8cc9cb585c884cd09953e066a9dc4151be96573
AlexValencia <alejandro.valencia.alvarez14@gmail.com>
2023-09-05 23:49:35 -0600

    Se agrega el ejercicio trabajado en equipo donde se creo una funcion para condicional, utilizando el ejemplo del consultorio dental

--- controlFlujo.js
+++ controlFlujo.js
@@ -189,7 +189,7 @@ si es alergico: if
 tiene alguna condición: switch
 */
 
-let edad;
+
 
 function mayorEdad(edad) {
     if (edad>=60){
@@ -204,9 +204,51 @@ function mayorEdad(edad) {
 
 let alergia;
 function tratamiento (alergia){
-    if (alergia = true){
-        console.log("Contestar ek formulario de alergias");
+    if (alergia == 1){
+        console.log("Contestar el formulario de alergias");
     } else {
         console.log("Omitir formulario de alergias");
     }
 }
+
+let edad=25;
+
+function servicios(){
+    let opcionServicio = parseInt(prompt("¿Que tipo de tratamiento prefieres? \n 1. Evaluación y planificación de diseño de sonrisa (Gratis) \n 2. Limpieza dental \n 3. Extracción simple"));
+
+    //Funcion principal SWITCH que llama a las funciones mayorEdad y tratamiento
+    //En cada uno de los casos se llama a estas funciones, mostrando su respectivo costo
+    //En DEFAULT muestra un error al no recibir un caso de los establecidos
+    switch (opcionServicio){
+        case 1:
+            console.log("Seleccionaste EVALUACIÓN \n El costo es GRATUITO") //Confirmacion de seleccion
+            let edad1 = parseInt(prompt("¿Cuantos anios tienes?"));
+            mayorEdad(edad1);
+            let alergia1 = parseInt(prompt("¿Cuentas con alguna alergia? \n Escribe 1 para responder SI \n Escribe otro número para responder NO"));  //Demostracion de opciones
+            tratamiento(alergia1);
+            break; //Se coloca esta linea para evitar el ciclo indefinido
+
+        case 2:
+            console.log("Seleccionaste LIMPIEZA \n El costo es $800") //Confirmacion de seleccion
+            let edad2 = parseInt(prompt("¿Cuantos anios tienes?"));
+            mayorEdad(edad2);
+            let alergia2 = parseInt(prompt("¿Cuentas con alguna alergia? \n Escribe 1 para responder SI \n Escribe otro número para responder NO")); //Demostracion de opciones
+            tratamiento(alergia2);
+            break;
+
+        case 3:
+            console.log("Seleccionaste Extracción \n El costo es $300") //Confirmacion de seleccion
+            let edad3 = parseInt(prompt("¿Cuantos anios tienes?"));
+            mayorEdad(edad3);
+            let alergia3 = parseInt(prompt("¿Cuentas con alguna alergia? \n Escribe 1 para responder SI \n Escribe otro número para responder NO")); //Demostracion de opciones
+            tratamiento(alergia3);
+            break;
+
+        default:
+            console.log("ERROR: No seleccionaste ninguna opcion"); //Mensaje error
+    }
+
+}
+
+//INICIAR PROCESO DE OBTENCION DE DATOS OFRECIENDO SERVICIOS
+servicios();
